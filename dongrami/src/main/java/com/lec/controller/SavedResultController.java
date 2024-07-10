6a76ea9798992ca32da4a7b9e9b775ffcaf9b2c5
Sohyeon <158130588+S0hye0nJung@users.noreply.github.com>
2024-07-11 08:59:28 +0900

    Update SavedResultController.java

--- dongrami/src/main/java/com/lec/controller/SavedResultController.java
+++ dongrami/src/main/java/com/lec/controller/SavedResultController.java
@@ -1,6 +1,7 @@
 package com.lec.controller;
 
 import java.text.ParseException;
+import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.lec.dto.SavedResultDTO;
 import com.lec.entity.Member;
@@ -19,22 +21,31 @@ import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class SavedResultController {
-	
-	@Autowired
-	SavedResultService savedResultService;
-	
-	@PostMapping("/save")
-	public String saveResult(@RequestParam("userId") String userId,
-							@RequestParam("webReadingId") int webReadingId,
-							@RequestParam("readingTitle") String readingTitle,
-							@RequestParam("subId") int subId,
-							@RequestParam("cardId") int cardId
-							) throws ParseException {
-		
-		
-		savedResultService.saveResult(userId, webReadingId, readingTitle, subId, cardId);
-		
-		return "redirect:/index";
-	}
-
+    
+    @Autowired
+    SavedResultService savedResultService;
+    
+    @PostMapping("/save")
+    @ResponseBody
+    public Map<String, Object> saveResult(@RequestParam("userId") String userId,
+                                          @RequestParam("webReadingId") int webReadingId,
+                                          @RequestParam("readingTitle") String readingTitle,
+                                          @RequestParam("subId") int subId,
+                                          @RequestParam("cardId") int cardId) throws ParseException {
+        
+        try {
+            savedResultService.saveResult(userId, webReadingId, readingTitle, subId, cardId);
+            Map<String, Object> response = new HashMap<>();
+            response.put("success", true);
+            response.put("message", "저장에 성공했습니다.");
+            return response;
+        } catch (Exception e) {
+            Map<String, Object> response = new HashMap<>();
+            response.put("success", false);
+            response.put("message", "저장에 실패했습니다: " + e.getMessage());
+            return response;
+        }
+    }
 }
+
+
