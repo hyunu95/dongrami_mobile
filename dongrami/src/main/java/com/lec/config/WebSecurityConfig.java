17b5ad6cea5a7b60c0d6c40cdea2c122e7485c31
Sohyeon <158130588+S0hye0nJung@users.noreply.github.com>
2024-07-10 18:24:14 +0900

    Update WebSecurityConfig.java

--- dongrami/src/main/java/com/lec/config/WebSecurityConfig.java
+++ dongrami/src/main/java/com/lec/config/WebSecurityConfig.java
@@ -10,8 +10,6 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.core.user.OAuth2User;
@@ -44,9 +42,8 @@ public class WebSecurityConfig {
            http
                .csrf().disable()
                .authorizeHttpRequests(request -> request
-                .requestMatchers("/checkLogin").permitAll()
-            	.requestMatchers("/review").authenticated()                 
-            	.requestMatchers("/mainvote/**").permitAll()
+            	.requestMatchers("/review").authenticated()                                 
+            	.requestMatchers("/api/replies/{voteId}").authenticated()
                    .anyRequest().permitAll()
                )
                .formLogin(form -> form
@@ -119,10 +116,5 @@ public class WebSecurityConfig {
 //            session.setAttribute("provider", provider);
             response.sendRedirect("/");
       }
-       
-       @Bean
-       public PasswordEncoder passwordEncoder() {
-           return new BCryptPasswordEncoder();
-       }
 }
  
