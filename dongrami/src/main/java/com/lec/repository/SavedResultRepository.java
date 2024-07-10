13ff30ce346fd8aaaeca7c91bb666247914f3f94
Sohyeon <158130588+S0hye0nJung@users.noreply.github.com>
2024-07-10 18:03:05 +0900

    Update SavedResultRepository.java

--- dongrami/src/main/java/com/lec/repository/SavedResultRepository.java
+++ dongrami/src/main/java/com/lec/repository/SavedResultRepository.java
@@ -1,6 +1,7 @@
 package com.lec.repository;
 
 import com.lec.entity.SavedResult;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SavedResultRepository extends JpaRepository<SavedResult, Integer> {
