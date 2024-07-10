e1f2d74f719244cf674a76041f60f62fc853217e
Sohyeon <158130588+S0hye0nJung@users.noreply.github.com>
2024-07-10 11:33:04 +0900

    Update PageController.java

--- dongrami/src/main/java/com/lec/controller/PageController.java
+++ dongrami/src/main/java/com/lec/controller/PageController.java
@@ -7,11 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class PageController {
    
-    
-    @GetMapping("/review")
-    public String reviewPage() {
-        return "review";
-    }
+   
     
     @GetMapping("/mypage")
     public String myPage() {
