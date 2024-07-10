cc755a26345ce0bdad0a4f402116421ccbfd5550
Sohyeon <158130588+S0hye0nJung@users.noreply.github.com>
2024-07-10 11:37:21 +0900

    Update ReviewRepository.java

--- dongrami/src/main/java/com/lec/repository/ReviewRepository.java
+++ dongrami/src/main/java/com/lec/repository/ReviewRepository.java
@@ -1,5 +1,17 @@
 package com.lec.repository;
 
-public interface ReviewRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.lec.entity.Review;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
+
+public interface ReviewRepository extends JpaRepository<Review, Integer> {
+
+    @Query("SELECT r FROM review r JOIN FETCH r.member m JOIN FETCH r.subcategory s")
+    List<Review> findAllWithDetails();
+
+    @Query("SELECT r FROM review r JOIN FETCH r.member m JOIN FETCH r.subcategory s WHERE s.maincategory.maincategory_id = :mainCategoryId")
+    List<Review> findByMainCategory(@Param("mainCategoryId") int mainCategoryId);
 }
