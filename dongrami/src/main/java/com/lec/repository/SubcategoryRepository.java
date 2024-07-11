e4135fa53d41a3bcb994b978221899b0b31d9cbd
Sohyeon <158130588+S0hye0nJung@users.noreply.github.com>
2024-07-11 16:01:03 +0900

    Update SubcategoryRepository.java

--- dongrami/src/main/java/com/lec/repository/SubcategoryRepository.java
+++ dongrami/src/main/java/com/lec/repository/SubcategoryRepository.java
@@ -1,13 +1,18 @@
 package com.lec.repository;
 
 import com.lec.entity.SavedResult;
+import com.lec.entity.Subcategory;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.*;
 
 import java.util.Optional;
 
 @Repository
-public interface SavedResultRepository extends JpaRepository<SavedResult, Integer> {
+public interface SubcategoryRepository extends JpaRepository<Subcategory, Integer> {
+	List<Subcategory> findTop5ByOrderByCountDesc();
+
     // 추가된 메서드
-    Optional<SavedResult> findById(Integer resultId);
+    Optional<Subcategory> findById(Integer subcategoryId);
 }
