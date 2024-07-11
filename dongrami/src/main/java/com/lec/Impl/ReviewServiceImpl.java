22501bac87a2a3625f91d2213a3de0ea39262c1f
Sohyeon <158130588+S0hye0nJung@users.noreply.github.com>
2024-07-11 09:06:26 +0900

    Update ReviewServiceImpl.java

--- dongrami/src/main/java/com/lec/Impl/ReviewServiceImpl.java
+++ dongrami/src/main/java/com/lec/Impl/ReviewServiceImpl.java
@@ -1,13 +1,16 @@
 package com.lec.Impl;
 
-import com.lec.entity.Review;
 import com.lec.dto.ReviewDTO;
+import com.lec.entity.Member;
+import com.lec.entity.Review;
+import com.lec.entity.Subcategory;
+import com.lec.repository.MemberRepository;
 import com.lec.repository.ReviewRepository;
+import com.lec.repository.SubcategoryRepository;
 import com.lec.service.ReviewService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -17,33 +20,58 @@ public class ReviewServiceImpl implements ReviewService {
     @Autowired
     private ReviewRepository reviewRepository;
 
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Autowired
+    private SubcategoryRepository subcategoryRepository;
+
     @Override
     public void saveReview(Review review) {
         reviewRepository.save(review);
     }
 
     @Override
-    public void updateReview(int id, ReviewDTO reviewDTO) {
+    public void saveReview(ReviewDTO reviewDTO) throws Exception {
+        Optional<Member> memberOptional = memberRepository.findById(reviewDTO.getUserId());
+        if (!memberOptional.isPresent()) {
+            throw new Exception("User not found");
+        }
+        Member member = memberOptional.get();
+
+        Optional<Subcategory> subcategoryOptional = subcategoryRepository.findById(reviewDTO.getSubcategoryId());
+        if (!subcategoryOptional.isPresent()) {
+            throw new Exception("Subcategory not found");
+        }
+        Subcategory subcategory = subcategoryOptional.get();
+
+        Review review = Review.builder()
+                .rating(reviewDTO.getRating())
+                .reviewText(reviewDTO.getReviewText())
+                .reviewCreate(new java.util.Date())
+                .member(member)
+                .subcategory(subcategory)
+                .build();
+        reviewRepository.save(review);
+    }
+
+    @Override
+    public void updateReview(int id, ReviewDTO reviewDTO) throws Exception {
         Optional<Review> optionalReview = reviewRepository.findById(id);
         if (optionalReview.isPresent()) {
-            Review existingReview = optionalReview.get();
-            existingReview.setRating(reviewDTO.getRating());
-            existingReview.setReviewText(reviewDTO.getReviewText());
-            existingReview.setReviewModify(new Date());
-            reviewRepository.save(existingReview);
+            Review review = optionalReview.get();
+            review.setRating(reviewDTO.getRating());
+            review.setReviewText(reviewDTO.getReviewText());
+            review.setReviewModify(new java.util.Date());
+            reviewRepository.save(review);
         } else {
-            throw new RuntimeException("Review not found with id " + id);
+            throw new Exception("Review not found");
         }
     }
 
     @Override
     public void deleteReview(int id) {
-        Optional<Review> optionalReview = reviewRepository.findById(id);
-        if (optionalReview.isPresent()) {
-            reviewRepository.delete(optionalReview.get());
-        } else {
-            throw new RuntimeException("Review not found with id " + id);
-        }
+        reviewRepository.deleteById(id);
     }
 
     @Override
@@ -54,5 +82,16 @@ public class ReviewServiceImpl implements ReviewService {
     @Override
     public List<Review> getReviewsByMainCategory(int mainCategoryId) {
         return reviewRepository.findByMainCategory(mainCategoryId);
+    }
+
+    @Override
+    public ReviewDTO getReviewById(int id) {
+        Optional<Review> optionalReview = reviewRepository.findById(id);
+        if (optionalReview.isPresent()) {
+            Review review = optionalReview.get();
+            return new ReviewDTO(review);
+        } else {
+            return null;
+        }
     }
 }
