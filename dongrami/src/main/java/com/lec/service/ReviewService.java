1b7fe2b48408086acd1842ed2f95433885acf3e4
Sohyeon <158130588+S0hye0nJung@users.noreply.github.com>
2024-07-11 09:23:26 +0900

    Update ReviewService.java

--- dongrami/src/main/java/com/lec/service/ReviewService.java
+++ dongrami/src/main/java/com/lec/service/ReviewService.java
@@ -1,14 +1,16 @@
 package com.lec.service;
 
-import com.lec.entity.Review;
 import com.lec.dto.ReviewDTO;
+import com.lec.entity.Review;
 
 import java.util.List;
 
 public interface ReviewService {
     void saveReview(Review review);
-    void updateReview(int id, ReviewDTO reviewDTO);
+    void saveReview(ReviewDTO reviewDTO) throws Exception;
+    void updateReview(int id, ReviewDTO reviewDTO) throws Exception;
     void deleteReview(int id);
     List<Review> getAllReviews();
     List<Review> getReviewsByMainCategory(int mainCategoryId);
+    ReviewDTO getReviewById(int id);
 }
