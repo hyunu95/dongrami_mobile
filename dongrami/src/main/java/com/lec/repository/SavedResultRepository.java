592f6f99b86c6058625f834d6819c290024307e0
Sohyeon <158130588+S0hye0nJung@users.noreply.github.com>
2024-07-11 09:21:39 +0900

    Update SavedResultRepository.java

--- dongrami/src/main/java/com/lec/repository/SavedResultRepository.java
+++ dongrami/src/main/java/com/lec/repository/SavedResultRepository.java
@@ -2,8 +2,15 @@ package com.lec.repository;
 
 import com.lec.entity.SavedResult;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface SavedResultRepository extends JpaRepository<SavedResult, Integer> {
-
-	void deleteByMemberUserId(String userId);
+    // 추가된 메서드
+    Optional<SavedResult> findById(Integer resultId);
 }
+
+
+// 원래는 string->integer로 바뀜 
